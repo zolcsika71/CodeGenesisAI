121e281df058bbf08edbabd165de5a0819156e19
zolcsika71 <bushdoctor2008@gmail.com>
2023-10-22 03:35:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+This script to create an autonomous self-improving python code writer and executor
+It is based on https://community.openai.com/t/infantagi-an-autonomous-self-improving-python-code-writer-and-executer/240242
+"""
+
+
+def run():
+    """
+    This is the main function
+    """
+    print("The Main script is running")
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    run()
